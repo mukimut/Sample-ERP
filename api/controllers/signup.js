cbe19148d8fef02d094fa0a2a44bfa43a6a166f7
samin <saminnnn@gmailcom>
2019-05-11 22:32:00 +0600

    Fixed error with updating

--- api/controllers/signup.js
+++ api/controllers/signup.js
@@ -3,18 +3,24 @@ module.exports = async function signup(req, res) {
 
 	const users = await User.find({username: username});
 
-	if(req.body.userid) {
-		await User.update({id: req.body.userid}).set({username: req.body.username, password: req.body.password});
-		res.send({update: true});
-	}
-	else if(users.length === 0){
-		const token = await sails.helpers.tokenSource();
-		let userData = {username: username, password: req.body.password, token: token}
-		if(req.body.access) userData.access = req.body.access;
-		await User.create(userData);
-		res.send({unique: true, token: token});
+	if(users.length === 0){
+        let userData = {username: username, password: req.body.password};
+        if(req.body.access) userData.access = req.body.access;
+        let update;
+
+        if(req.body.userid) {
+            await User.update({id: req.body.userid}).set(userData);
+            update = true;
+            res.send({update: true});
+        }
+        else {
+            userData.token = await sails.helpers.tokenSource();
+            await User.create(userData);
+            update = true;
+            res.send({unique: true, token: token}); 
+        }
 	}
 	else {
-		res.send({unique: false});
+		res.send({unique: false, update: false, message: 'Username already exists'});
 	}
 }
