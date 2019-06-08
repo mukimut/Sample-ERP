46a350422da759e99b35be7940941a02e1e4b712
saminnnn <saminnnn@gmail.com>
2019-06-08 23:33:54 +0600

    Upgrades to mapping page

--- api/models/TOV.js
+++ api/models/TOV.js
@@ -5,3 +5,20 @@ module.exports = {
 	},
 	tableName: 'tov'
 }
+
+/*
+CREATE TABLE public.tov
+(
+    valuetype text NOT NULL,
+    "values" text[],
+    PRIMARY KEY (valuetype)
+)
+WITH (
+    OIDS = FALSE
+);
+
+ALTER TABLE public.tov
+    OWNER to postgres;
+
+GRANT ALL ON TABLE public.tov TO samin;
+*/
