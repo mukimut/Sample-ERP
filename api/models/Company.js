85f7745aab4cdddc4f44ec2019e414a51834cae6
saminnnn <saminnnn@gmail.com>
2019-06-16 23:26:47 +0600

    Company data inserting

--- api/models/Company.js
+++ api/models/Company.js
@@ -4,14 +4,13 @@ module.exports = {
 		name: {type: 'string', required: true},
 		shortname: {type: 'string', required: true},
 		phone: {type: 'string'},
-		mobile: {type: 'string'},
+		companytype: {type: 'string'},
 		email: {type: 'string'},
 		website: {type: 'string'},
-		address: {type: 'string'},
-		district: {type: 'string'},
+		address: {type: 'json'},
 		companytype: {type: 'string'},
-		postcode: {type: 'number'},
-		lca: {type: 'string'},
+		tin: {type: 'string'},
+		contact: {type: 'json'}
 },
 tableName: 'company'
 }
@@ -44,4 +43,17 @@ ALTER TABLE public.company
     OWNER to postgres;
 
 GRANT ALL ON TABLE public.company TO samin;
+
+ALTER TABLE public.company DROP COLUMN address;
+
+ALTER TABLE public.company
+    ADD COLUMN address jsonb;
+
+ALTER TABLE public.company
+	ADD COLUMN contact jsonb;
+ALTER TABLE public.company DROP COLUMN mobile;
+ALTER TABLE public.company DROP COLUMN district;
+ALTER TABLE public.company DROP COLUMN postcode;
+ALTER TABLE public.company
+    RENAME lca TO tin;
 */
