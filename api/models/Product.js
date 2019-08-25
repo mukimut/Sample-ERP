889cf79a1a3d373075ac70511cc38128564f5740
saminnnn <saminnnn@gmail.com>
2019-08-25 22:16:48 +0600

    Modeling Product

---
+++ api/models/Product.js
@@ -0,0 +1,27 @@
+module.exports = {
+    attributes: {
+        id: {type: 'number', columnName: 'productId', autoIncrement: true},
+        quantity: {type: 'number'},
+        price: {type: 'number'},
+        name: {type: 'string', required: true},
+        unit: {type: 'string', required: true},
+        
+    },
+    tableName: 'product'
+}
+
+/*
+CREATE TABLE public.product
+(
+    "productId" bigint NOT NULL DEFAULT nextval('"product_productId_seq"'::regclass),
+    name text COLLATE pg_catalog."default",
+    unit text COLLATE pg_catalog."default",
+    quantity numeric,
+    price money,
+    CONSTRAINT product_pkey PRIMARY KEY ("productId")
+)
+WITH (
+    OIDS = FALSE
+)
+
+*/
