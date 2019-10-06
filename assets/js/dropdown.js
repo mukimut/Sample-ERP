40cdc64b589ad51b127845d9592225bc1659c78b
saminnnn <saminnnn@gmail.com>
2019-10-07 00:31:00 +0600

    Fixed Company Bugs and Dropdown

--- assets/js/dropdown.js
+++ assets/js/dropdown.js
@@ -1,42 +1,40 @@
 function hello() {
-    console.log('Helllo');
+  console.log('Helllo');
 }
 
 function setSelectionList(id, inputList, title=null) {
-    if(!title) title = 'Select Type';
-    let string = '';
-    list = ['<option hidden disabled selected value>' + title + '</option>'];
-    if(inputList.length > 0) list = list.concat(inputList)
-    list.forEach(element => string += '<option>' + element + '</option>');
-    document.getElementById(id).innerHTML = string;
+	if(!title) title = 'Select Type';
+	let string = '<option hidden disabled selected value>' + title + '</option>';
+	inputList.forEach(element => string += '<option>' + element + '</option>');
+	document.getElementById(id).innerHTML = string;
 }
 
 function getSelectedValues(selectList) {
-    selectList.forEach(element => {
-        const selector = document.getElementById(element);
-        data[element] = selector.options[selector.selectedIndex].text;
-    });
-    return data;
+	selectList.forEach(element => {
+		const selector = document.getElementById(element);
+		data[element] = selector.options[selector.selectedIndex].text;
+	});
+	return data;
 }
 
 function getSelectedText(id) {
-    const selection = document.getElementById(id);
-    const selectedIndex = selection.selectedIndex;
-    if(selectedIndex === -1) return null;
-    return selection.options[selectedIndex].innerHTML;
-  }
+	const selection = document.getElementById(id);
+	const selectedIndex = selection.selectedIndex;
+	if(selectedIndex === -1) return null;
+	return selection.options[selectedIndex].innerHTML;
+}
 
 function setSelectedValue(id, value) {
-    const selection = document.getElementById(id);
-    const options = selection.options;
-    const length = options.length;
+	const selection = document.getElementById(id);
+	const options = selection.options;
+	const length = options.length;
 
-    for (let i = 0; i < length; i++) {
-        if (options[i].innerHTML === value) {
-            selection.selectedIndex = i;
-            break;
-        }
-    }
+	for (let i = 0; i < length; i++) {
+		if (options[i].innerHTML === value) {
+			selection.selectedIndex = i;
+			break;
+		}
+	}
 }
 
 function getSelectedIndex(id) {
