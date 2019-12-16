270c609c409c1fe1d1e21f24bcbcae841f897d16
saminnnn <saminnnn@gmail.com>
2019-12-17 01:02:46 +0600

    Report Queries

--- api/controllers/expenses/get-expense.js
+++ api/controllers/expenses/get-expense.js
@@ -1,6 +1,41 @@
 module.exports = async function getExpenses(req, res) {
   const status = req.query.status;
-  const statusList = JSON.parse('[' + status + ']');
-  const expenses = await Expense.find({approved: statusList});
-  res.send(expenses);
+  if(status) {
+    const statusList = JSON.parse('[' + status + ']');
+    const expenses = await Expense.find({approved: statusList});
+    res.send(expenses);
+  }
+  else {
+    let filterList = [];
+    let query = 'SELECT expense_id, type, project, amount, purpose, date, invoice, approved FROM public.expenses';
+    let filter = ' WHERE 1=1 ';
+    let added = 1;
+
+    if(req.query.project) {
+      filter += ` AND project = $` + added;
+      added++;
+      filterList.push(req.query.project)
+    }
+    if(req.query.invoice) {
+      filter += ` AND invoice LIKE $` + added;
+      added++;
+      filterList.push('%' + req.query.invoice + '%');
+    }
+    if(req.query.from) {
+      filter += ` AND date >= $` + added;
+      added++;
+      filterList.push(req.query.from);
+    }
+    if(req.query.from) {
+      filter += ` AND date <= $` + added;
+      added++;
+      filterList.push(req.query.to);
+    }
+
+
+// console.log(query + filter);
+// console.log(filterList);
+    const rawResult = await sails.sendNativeQuery(query + filter, filterList);
+    res.send(rawResult.rows);
+  }
 }
