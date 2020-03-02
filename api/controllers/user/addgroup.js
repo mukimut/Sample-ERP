484d66461dde2a3df389f98c5cc73060f9cbfb79
saminnnn <saminnnn@gmail.com>
2020-03-03 00:15:47 +0600

    update and delete group

--- api/controllers/user/addgroup.js
+++ api/controllers/user/addgroup.js
@@ -1,22 +1,27 @@
 module.exports = async function addGroup(req, res) {
   const group = req.body.group;
   const pages = req.body.pages;
-  const user = req.body.user
+  const user = req.body.user;
+  const newGroupName = req.body.newName;
 
-  if(!pages) {
+  if(!pages && !newGroupName) {
     await Permission.create({id: group});
     res.send({status: true});
   }
   else if(group){
-    const sql = `update permissions set pages = '{` + pages.join() + `}' where permissions.group = $1`
-    const rawResult = await sails.sendNativeQuery(sql, [group]);
+    const sql = `update permissions set pages = '{` + pages.join() + `}', "group" = $2 where permissions.group = $1`;
+    const rawResult = await sails.sendNativeQuery(sql, [group, newGroupName]);
 
     res.send({status: true});
   }
-  else {
+  else if(user){
     const sql = `update users set pages = '{` + pages.join() + `}' where userid = $1`
     const rawResult = await sails.sendNativeQuery(sql, [user]);
 
     res.send({status: true});
+  }
+  else {
+    await Permission.destroy({id: newGroupName});
+    res.send({update: true});
   }
 }
