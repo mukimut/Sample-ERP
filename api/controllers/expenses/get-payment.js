a546c462f0a47af55fb767cb6de6764e071cc9de
Staging Server <serve@innovior.com>
2020-05-16 16:17:45 +0600

    payment

--- api/controllers/expenses/get-payment.js
+++ api/controllers/expenses/get-payment.js
@@ -1,22 +1,16 @@
 module.exports = async function getPayment(req, res) {
-    // const paymentType = req.query.paymenttype;
-    // console.log(paymentType);
-    // const payments = await Payment.find({paymenttype: paymentType});
-    // res.send(payments);
-
-
-
 
     const fromDate = req.query.fromDate;
     const toDate = req.query.toDate;
     const paymentType = req.query.paymenttype;
 	
-		const sql = `select company, bank, brunch, checkdate, created, checkno, receipt, amount from payment where 
-        paymenttype='Party Payment' and 
-        created between '` + fromDate + `' and '` + toDate + `'` ;
+		// const sql = `select company, bank, brunch, checkdate, created, checkno, receipt, amount from payment where 
+    //     paymenttype='Party Payment' and 
+    //     created between '` + fromDate + `' and '` + toDate + `'` ;
+    const sql = `select company, bank, brunch, checkdate, created, checkno, receipt, amount, paymentmethod from payment where 
+        paymenttype='Party Payment' order by paymentid desc offset 0 rows fetch next 20 rows only` ;
 		const searchData = await sails.sendNativeQuery(sql, []);
-		res.send(searchData);
-
+		res.send(searchData.rows);
 
     //res.send(searchData);
     
