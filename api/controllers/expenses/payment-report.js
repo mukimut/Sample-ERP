602789a3f3a64ddc6af66d0e934e4acf9e7947dc
Staging Server <serve@innovior.com>
2020-06-04 23:45:39 +0600

    Payment Report

--- api/controllers/expenses/payment-report.js
+++ api/controllers/expenses/payment-report.js
@@ -2,7 +2,8 @@ module.exports = async function paymentReport(req, res) {
   const fromDate = req.query.fromDate;
   const toDate = req.query.toDate;
 
-  const sql = `select * from public.payment_report($1,$2);` ;
+  const sql = `select * from public.payment_report($1,$2, 0);` ;
+// console.log(sql)
   const searchData = await sails.sendNativeQuery(sql, [fromDate, toDate]);
-  res.send(searchData);
+  res.send(searchData.rows);
 }
